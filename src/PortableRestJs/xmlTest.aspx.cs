140b14a6138e259fda4434625a95e4b48d5e5dd6
Shmuel Englard <shmueli.yosef@englard.net>
2013-02-25 13:07:50 -0500

    Fixed some bugs in JS version and added JS tests

---
+++ src/PortableRestJs/xmlTest.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+
+namespace PortableRestJs
+{
+    public partial class xmlTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Clear();
+            string body = string.Empty;
+            using (StreamReader streamReader = new StreamReader(Request.InputStream))
+            {
+                body = streamReader.ReadToEnd();
+            }
+
+            XDocument xDoc = JsonConvert.DeserializeXNode(body);
+
+            Response.Write(xDoc.ToString(SaveOptions.DisableFormatting));
+            Response.ContentType = "application/xml";
+            Response.Headers.Add("Content-Type", Response.ContentType);
+        }
+    }
+}
