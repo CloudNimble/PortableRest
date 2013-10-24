833f62100e70d637c07553cf430086173a5ca3a9
advancedrei <robert@advancedrei.com>
2013-10-23 23:34:20 -0400

    - Adjustments to the AssemblyInfo file.

--- src/PortableRest/Properties/AssemblyInfo.cs
+++ src/PortableRest/Properties/AssemblyInfo.cs
@@ -1,8 +1,5 @@
 ﻿using System.Reflection;
 using System.Resources;
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("PortableRest")]
@@ -25,6 +22,6 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: InternalsVisibleTo("PortableRest.Tests")]
