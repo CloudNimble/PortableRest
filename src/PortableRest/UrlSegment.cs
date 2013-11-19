1e3e90813c912a218e5c561926c8881dea18aaa6
advancedrei <robert@advancedrei.com>
2013-11-19 14:09:36 -0500

    - 2.1 RTM.

---
+++ src/PortableRest/UrlSegment.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortableRest
+{
+    internal class UrlSegment
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsQueryString { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public UrlSegment(string key, string value)
+        {
+            Key = key;
+            Value = value;
+            IsQueryString = false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="isQueryString"></param>
+        public UrlSegment(string key, string value, bool isQueryString)
+        {
+            Key = key;
+            Value = value;
+            IsQueryString = isQueryString;
+        }
+
+    }
+}
